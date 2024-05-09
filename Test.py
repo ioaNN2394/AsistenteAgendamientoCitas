72caa43ad828b6cf9826f7c19ea7a84b9f93a40f
Johan Quimbayo <johansa2394@gmail.com>
2024-05-09 11:34:39 -0500

    p

--- Test.py
+++ Test.py
@@ -1,47 +1,47 @@
+import os
 import unittest
-import pymongo
-from unittest.mock import patch, MagicMock
-from pymongo.errors import ServerSelectionTimeoutError, ConnectionFailure
-from AccesoDatos.Mongo_Connection import MongoConnection
+from unittest.mock import patch
 
-from Infraestructura.langchain_tools import PatientInfoChecker, VerifyPatientInfoChecker, SendEmail, VerifyDoctorMPatient, SendPatientInfo, VerifyPatientInfo, InformPsychologist
-from Infraestructura.models import Chat, ChatStatus
-from Infraestructura.langchain_tools import _PatientInfo, _QuotetInfo, DoctorMPatient
-from langchain_core.callbacks import CallbackManagerForToolRun
+from dotenv import load_dotenv
 
 from AccesoDatos.patient_model import PatientModel
-from Infraestructura.langchain_tools import _PatientInfo
-
-from Infraestructura.AvoidCircularImport import _PatientInfo
 from Infraestructura.agenda import GoogleCalendarManager
-
-import os
+from Infraestructura.langchain_tools import (
+    PatientInfoChecker, SendEmail, VerifyDoctorMPatient, SendPatientInfo,
+    VerifyPatientInfo, InformPsychologist, _QuotetInfo, DoctorMPatient,
+    _PatientInfo
+)
+from Infraestructura.models import Chat, ChatStatus, Message, SenderEnum
+from LogicaNegocio import langchain_executor
 from LogicaNegocio.langchain_executor import invoke
-from dotenv import load_dotenv
 load_dotenv()  # take environment variables from .env.
 
-from LogicaNegocio import langchain_executor
-from Infraestructura.models import Chat, Message, SenderEnum
 
-#----------------------------------Pruebas Integracion----------------------------------
-
+# ----------------------------------Pruebas Integracion----------------------------------
 
 class TestSendEmail(unittest.TestCase):
     def setUp(self):
-        self.patient_info = _PatientInfo(name="John Doe", age=30, motive="Consultation", country="CANADA", date="2022-12-12")
-
+        self.patient_info = _PatientInfo(
+            name="John Doe", age=30, motive="Consultation", country="CANADA", date="2022-12-12"
+        )
 
     @patch('smtplib.SMTP_SSL')
     def test_send_email_to_patient(self, mock_smtp):
-        patient_info = _PatientInfo(name="John Doe", age=30, motive="Consultation", country="CANADA", date="2022-12-12")
+        patient_info = _PatientInfo(
+            name="John Doe", age=30, motive="Consultation", country="CANADA", date="2022-12-12"
+        )
         email_sender = SendEmail(patient_info)
         result, error = email_sender.send()
         self.assertTrue(result)
         self.assertIsNone(error)
+
+
 class TestPatientModel(unittest.TestCase):
     def setUp(self):
         self.patient_model = PatientModel()
-        self.patient_info = _PatientInfo(name="John Doe", age=30, motive="Consultation", country="USA", date="2022-12-12")
+        self.patient_info = _PatientInfo(
+            name="John Doe", age=30, motive="Consultation", country="USA", date="2022-12-12"
+        )
 
     def test_insert_patient_into_database(self):
         # Insert the test patient into the database
@@ -57,10 +57,13 @@ class TestPatientModel(unittest.TestCase):
         self.assertEqual(patient_in_db["country"], self.patient_info.country)
         self.assertEqual(patient_in_db["date"], self.patient_info.date)
 
+
 class TestGoogleCalendarManager(unittest.TestCase):
     def setUp(self):
         self.patient_model = PatientModel()
-        self.patient_info = _PatientInfo(name="Test Name", motive="TEST", date="2024-5-5", age=30, country="Country Name")
+        self.patient_info = _PatientInfo(
+            name="Test Name", motive="TEST", date="2024-5-5", age=30, country="Country Name"
+        )
         self.calendar = GoogleCalendarManager()
 
     def test_create_calendar_event_for_patient(self):
@@ -74,11 +77,11 @@ class TestGoogleCalendarManager(unittest.TestCase):
         # Test create_patient_event
         self.calendar.create_patient_event(self.patient_info)
 
-#----------------------------------Pruebas Unitarias----------------------------------
 
+# ----------------------------------Pruebas Unitarias----------------------------------
 
 class TestOpenAIConnection(unittest.TestCase):
-    @patch('os.environ', {'OPENAI_API_KEY': 'sk-proj-nx3giY95WVsYxTUfhF9WT3BlbkFJC5qBrh6jbzFXTAq4lg0W'})
+    @patch('os.environ', {'OPENAI_API_KEY': 'sk-proj-exRLI3SNdIMSidMMx3voT3BlbkFJhFCVAWG978SVjVboGfIu'})
     def test_openai_connection(self):
         # Create a test chat
         chat = Chat()
@@ -88,57 +91,84 @@ class TestOpenAIConnection(unittest.TestCase):
 
         # Check if the response is not empty
         self.assertIsNotNone(response)
-#Tools
+
+
+# Tools
 
 class TestPatientInfoChecker(unittest.TestCase):
     def setUp(self):
-        self.patient_info = _PatientInfo(name="John Doe", age=30, motive="Consultation", country="USA", date="2022-12-12")
+        self.patient_info = _PatientInfo(
+            name="John Doe", age=30, motive="Consultation", country="USA", date="2022-12-12"
+        )
 
     def test_patient_info_completeness(self):
         checker = PatientInfoChecker(self.patient_info)
         self.assertTrue(checker.is_info_complete())
 
     def test_doctor_patient_info_completeness(self):
-        verify_all_info = DoctorMPatient(MeetPatient=True, AllInfo="All info", name="John Doe", age=30, motive="Consultation", country="USA", date="2022-12-12")
+        verify_all_info = DoctorMPatient(
+            MeetPatient=True, AllInfo="All info", name="John Doe", age=30, motive="Consultation", country="USA", date="2022-12-12"
+        )
         self.assertTrue(VerifyDoctorMPatient.is_info_complete(verify_all_info))
+
 
 class TestSendPatientInfo(unittest.TestCase):
     def setUp(self):
         self.chat_history = Chat(status=ChatStatus.status1)
-        self.patient_info = _PatientInfo(name="John Doe", age=30, motive="Consultation", country="USA", date="2022-12-12")
+        self.patient_info = _PatientInfo(
+            name="John Doe", age=30, motive="Consultation", country="USA", date="2022-12-12"
+        )
         self.tool = SendPatientInfo(chat_history=self.chat_history)
 
     def test_send_patient_info_run(self):
-        result = self.tool._run(name=self.patient_info.name, age=self.patient_info.age, motive=self.patient_info.motive, country=self.patient_info.country, date=self.patient_info.date)
+        result = self.tool._run(
+            name=self.patient_info.name, age=self.patient_info.age, motive=self.patient_info.motive,
+            country=self.patient_info.country, date=self.patient_info.date
+        )
         self.assertEqual(result, "Hola otra vez, para continuar con el agendamiento de la cita envía (Continuar)")
+
 
 class TestVerifyPatientInfo(unittest.TestCase):
     def setUp(self):
         self.chat_history = Chat(status=ChatStatus.status2)
-        self.verify_patient = _QuotetInfo(PatienData=True, payment_method="Cash", agrees_to_policies=True, name="John Doe", age=30, motive="Consultation", country="USA", date="2022-12-12")
+        self.verify_patient = _QuotetInfo(
+            PatienData=True, payment_method="Cash", agrees_to_policies=True, name="John Doe", age=30,
+            motive="Consultation", country="USA", date="2022-12-12"
+        )
         self.tool = VerifyPatientInfo(chat_history=self.chat_history)
 
     def test_verify_patient_info_run(self):
-        result = self.tool._run(PatienData=self.verify_patient.PatienData, payment_method=self.verify_patient.payment_method, agrees_to_policies=self.verify_patient.agrees_to_policies, name=self.verify_patient.name, age=self.verify_patient.age, motive=self.verify_patient.motive, country=self.verify_patient.country, date=self.verify_patient.date)
+        result = self.tool._run(
+            PatienData=self.verify_patient.PatienData, payment_method=self.verify_patient.payment_method,
+            agrees_to_policies=self.verify_patient.agrees_to_policies, name=self.verify_patient.name,
+            age=self.verify_patient.age, motive=self.verify_patient.motive, country=self.verify_patient.country,
+            date=self.verify_patient.date
+        )
         self.assertEqual(result, "Hola doctora Mariana ya tengo la información del paciente")
+
 
 class TestInformPsychologist(unittest.TestCase):
     def setUp(self):
         self.chat_history = Chat(status=ChatStatus.status3)
-        self.verify_all_info = DoctorMPatient(MeetPatient=True, AllInfo="All info", name="John Doe", age=30, motive="Consultation", country="USA", date="2022-12-12")
+        self.verify_all_info = DoctorMPatient(
+            MeetPatient=True, AllInfo="All info", name="John Doe", age=30, motive="Consultation", country="USA", date="2022-12-12"
+        )
         self.tool = InformPsychologist(chat_history=self.chat_history)
 
     def test_inform_psychologist_run(self):
-        result = self.tool._run(MeetPatient=self.verify_all_info.MeetPatient, AllInfo=self.verify_all_info.AllInfo, name=self.verify_all_info.name, age=self.verify_all_info.age, motive=self.verify_all_info.motive, country=self.verify_all_info.country, date=self.verify_all_info.date)
+        result = self.tool._run(
+            MeetPatient=self.verify_all_info.MeetPatient, AllInfo=self.verify_all_info.AllInfo,
+            name=self.verify_all_info.name, age=self.verify_all_info.age, motive=self.verify_all_info.motive,
+            country=self.verify_all_info.country, date=self.verify_all_info.date
+        )
         self.assertEqual(result, "Ya tengo la respuesta de la Doctora Mariana, gracias por tu tiempo.")
 
 #----------------------------------Prueba End to End----------------------------------
 
-
 class TestEndToEndStatus1(unittest.TestCase):
     def setUp(self):
         # Establecer la llave de la API como una variable de entorno
-        os.environ['OPENAI_API_KEY'] = 'sk-proj-nx3giY95WVsYxTUfhF9WT3BlbkFJC5qBrh6jbzFXTAq4lg0W'
+        os.environ['OPENAI_API_KEY'] = 'sk-proj-exRLI3SNdIMSidMMx3voT3BlbkFJhFCVAWG978SVjVboGfIu'
 
     def test_status1(self):
         # Crear una instancia de chat con el estado inicial
